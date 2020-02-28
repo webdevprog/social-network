44650b42db3e820ccc44b255bff353daebdd47ce
Vitatiy <targetdeveloper57@gmail.com>
2020-02-28 10:54:12 +0300

    styling dialogs

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,11 +1,38 @@
 
 import React from 'react';
-import classes from './dialogs.module.scss';
+import cls from './dialogs.module.scss';
 
-const Dialogs = () => {
+const Dialogs = (handelClick) => {
+    handelClick = (e) => {
+        let target = e.target;
+    
+        alert(target.textContent);
+        return target.textContent;
+    }
     return (
-        <div>
-            Dialogs
+        <div className={cls.dialogs}>
+            <div className={cls.dialogsItem}>
+                <div className={cls.dialog}>
+                    Vitaliy
+                </div>
+                <div className={cls.dialog}>
+                    Sasha
+                </div>
+                <div className={cls.dialog}>
+                    Viktoria
+                </div>
+            </div>
+            <div className={cls.messages}>
+                <div className={cls.message}>
+                    hi, Vasya
+                </div>
+                <div className={cls.message}>
+                    How are you?
+                </div>
+                <div className={cls.message}>
+                    What's up man.
+                </div>
+            </div>
         </div>
     );
 }
