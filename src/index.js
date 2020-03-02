4e05b419b35900c982727843fb213be8433588d9
Vitatiy <targetdeveloper57@gmail.com>
2020-03-02 16:15:26 +0300

    model store Redux

--- src/index.js
+++ src/index.js
@@ -2,13 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App';
-import state from './redux/state';
-import {addPost, updateTextPost, subscribe} from './redux/state';
+import store from './redux/state';
 
 let rerenderTree = (state) => {
-    ReactDOM.render(<App state={state} addPost={addPost} updateTextPost={updateTextPost}  />, document.getElementById('root'));
+    console.log('index.js Render');
+    ReactDOM.render(<App 
+    state={state} 
+    addPost={store.addPost.bind(store)} 
+    updateTextPost={store.updateTextPost.bind(store)}     
+    />, document.getElementById('root'));
 }
 
-rerenderTree(state);
+rerenderTree(store.getState());
 
-subscribe(state);
+store.subscribe(rerenderTree);
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,59 +1,66 @@
-let rerenderTree = () => {
-    console.log('State changed')
-}
+let store = {
+    _state: {
+        profilePage: {
+            textPost: '',
+            posts: [
+                { id: 1, message: "Hello, everyone", countLike: 3 },
+                { id: 2, message: "Hello, everybody", countLike: 4 },
+                { id: 3, message: "This is my code.", countLike: 8 },
+            ]
+        },
+        dialogsPage: {
+            diaologs: [
+                { name: 'Vitaiy', id: 1, img: 'https://cdn.pixabay.com/photo/2016/06/14/00/14/cat-1455468_960_720.jpg' },
+                { name: 'Sasha', id: 2, img: 'https://cdn.pixabay.com/photo/2016/06/14/00/14/cat-1455468_960_720.jpg' },
+                { name: 'Peter', id: 3, img: 'https://cdn.pixabay.com/photo/2016/06/14/00/14/cat-1455468_960_720.jpg' },
+                { name: 'Katya', id: 4, img: 'https://cdn.pixabay.com/photo/2016/06/14/00/14/cat-1455468_960_720.jpg' },
+            ],
+            messages: [
+                { message: "hi, Vasya", id: 1 },
+                { message: "How are you?", id: 2 },
+                { message: "What's up man.", id: 3 },
+                { message: "Are you crazy?", id: 4 },
+                { message: "no", id: 5 }
+            ],
+        },
+        sidebar: {
+            friends: [
+                { id: 1, name: 'Valya', img: 'https://cdn.pixabay.com/photo/2016/06/14/00/14/cat-1455468_960_720.jpg' },
+                { id: 2, name: 'Lera', img: 'https://cdn.pixabay.com/photo/2016/06/14/00/14/cat-1455468_960_720.jpg' },
+                { id: 3, name: 'Valera', img: 'https://cdn.pixabay.com/photo/2016/06/14/00/14/cat-1455468_960_720.jpg' },
+                { id: 4, name: 'Anna', img: 'https://cdn.pixabay.com/photo/2016/06/14/00/14/cat-1455468_960_720.jpg' },
+            ]
+        }
+    },
 
-let state = {
-    profilePage: {
-        textPost: '',
-        posts: [
-            { id: 1, message: "Hello, everyone", countLike: 3 },
-            { id: 2, message: "Hello, everybody", countLike: 4 },
-            { id: 3, message: "This is my code.", countLike: 8 },
-        ]
+    _callSubscriber() {
+        console.log('State was changed');
     },
-    dialogsPage: {
-        diaologs: [
-            { name: 'Vitaiy', id: 1, img: 'https://cdn.pixabay.com/photo/2016/06/14/00/14/cat-1455468_960_720.jpg' },
-            { name: 'Sasha', id: 2, img: 'https://cdn.pixabay.com/photo/2016/06/14/00/14/cat-1455468_960_720.jpg' },
-            { name: 'Peter', id: 3, img: 'https://cdn.pixabay.com/photo/2016/06/14/00/14/cat-1455468_960_720.jpg' },
-            { name: 'Katya', id: 4, img: 'https://cdn.pixabay.com/photo/2016/06/14/00/14/cat-1455468_960_720.jpg' },
-        ],
-        messages: [
-            { message: "hi, Vasya", id: 1 },
-            { message: "How are you?", id: 2 },
-            { message: "What's up man.", id: 3 },
-            { message: "Are you crazy?", id: 4 },
-            { message: "no", id: 5 }
-        ],
+
+    getState() {
+        return this._state;
     },
-    sidebar: {
-        friends: [
-            {id: 1, name: 'Valya', img: 'https://cdn.pixabay.com/photo/2016/06/14/00/14/cat-1455468_960_720.jpg'},
-            {id: 2, name: 'Lera', img: 'https://cdn.pixabay.com/photo/2016/06/14/00/14/cat-1455468_960_720.jpg'},
-            {id: 3, name: 'Valera', img: 'https://cdn.pixabay.com/photo/2016/06/14/00/14/cat-1455468_960_720.jpg'},
-            {id: 4, name: 'Anna', img: 'https://cdn.pixabay.com/photo/2016/06/14/00/14/cat-1455468_960_720.jpg'},
-        ]
+
+    addPost() {
+        let newObj = {
+            id: 5,
+            message: this._state.profilePage.textPost,
+            countLike: 4,
+        }
+        this._state.profilePage.posts.push(newObj);
+        this._state.profilePage.textPost = '';
+        this._callSubscriber(this._state);
+    },
+
+    updateTextPost(value) {
+        console.log(value)
+        this._state.profilePage.textPost = value;
+        this._callSubscriber(this._state);
+    },
+
+    subscribe(observer) {
+        this._callSubscriber = observer;
     }
 }
 
-export const addPost = (() => {
-    let newObj = {
-        id: 5,
-        message: state.profilePage.textPost,
-        countLike: 4,
-    }
-    state.profilePage.posts.push(newObj);
-    state.profilePage.textPost = '';
-    rerenderTree(state);
-})
-
-export const updateTextPost = ((textPost) => {
-    state.profilePage.textPost = textPost;
-    rerenderTree(state);
-})
-
-export const subscribe = (observer) => {
-    rerenderTree = observer;
-}
-
-export default state;
+export default store;
