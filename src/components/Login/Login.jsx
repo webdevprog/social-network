c4d906aec57f2a0ef13571d24acfb12405772e3d
Vitatiy <targetdeveloper57@gmail.com>
2020-08-09 16:41:40 +0300

    redux form comp

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,11 +1,53 @@
 import React from 'react';
+import { Field, reduxForm } from 'redux-form';
+import { connect } from 'react-redux';
+
+class LoginContainer extends React.Component {
+
+    render() {
+        return (
+            <Login />
+        )
+    }
+}
 
 const Login = (props) => {
     return (
         <div>
             <h1>Login</h1>
+            <ReduxFormLogin />
         </div>
     )
-} 
+}
 
-export default Login;
+let LoginForm = (props) => {
+    return (
+        <form onSubmit={props.handleSubmit}>
+            <div>
+                <Field placeholder="login" name="Login" component="input" type="text" />
+            </div>
+            <div>
+                <Field placeholder="password" name="password" component="input" type="password" />
+            </div>
+            <div>
+                <Field name="remember" component="input" type="checkbox" />
+                <label htmlFor="remember">Remember me</label>
+            </div>
+            <div>
+                <button type="submit">Login</button>
+            </div>
+        </form>
+    )
+}
+
+const ReduxFormLogin = reduxForm({
+    form: 'login'
+})(LoginForm);
+
+let mapStateToProps = (state) => {
+    return {
+        loginData: ''
+    }
+}
+
+export default connect(mapStateToProps, {})(LoginContainer);
--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -1,5 +1,6 @@
 import { createStore, combineReducers, applyMiddleware } from "redux";
 import thunk from 'redux-thunk';
+import { reducer as formReducer } from 'redux-form'
 import profileReducer from "./profile-reducer";
 import dialogsReducer from "./dialogs-reducer";
 import sidebarReducer from "./sidebar-reducer";
@@ -13,6 +14,7 @@ let reducers = combineReducers({
     usersPage: usersReducer,
     sidebar: sidebarReducer,
     auth: authReducer,
+    form: formReducer
 });
 
 let store = createStore(reducers, applyMiddleware(thunk));
